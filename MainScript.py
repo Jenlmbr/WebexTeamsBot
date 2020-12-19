78f87b81b3246a052eca12da99b0cebffa2139e9
Jenlmbr <54675613+Jenlmbr@users.noreply.github.com>
2020-12-19 16:05:24 +0100

    eureka!

--- MainScript.py
+++ MainScript.py
@@ -1,30 +1,28 @@
-import json 
-import requests 
+import requests
+import json
 import time
+accessToken = "Bearer ZDM1MDRiOGEtN2ZkYi00YWI0LTkxYjktYmMzYTdiZWUwYzQwODQ0ODAwNjUtZjZm_PF84_consumer"
 
-requests.packages.urllib3.disable_warnings()
-accessToken = "Bearer YjJjNDU2NGMtMDBlYS00MzAxLTgwNTgtMjQ5NjA4NjU1MGZkMDE2ZDI4MzQtNTNm_PF84_consumer"
+
+
+	
+
 
 r = requests.get(   "https://api.ciscospark.com/v1/rooms",
                     headers = {"Authorization": accessToken}
                 )
 
-# Check response from the API call 
-
 if not r.status_code == 200:
     raise Exception("Incorrect reply from Webex Teams API. Status code: {}. Text: {}".format(r.status_code, r.text))
 
-
-#check If Room is available, and display it
 
 print("List of rooms:")
 rooms = r.json()["items"]
 for room in rooms:
     print (room["title"])
-
-
-    # Roomname
-    roomNameToSearch = "IP_address"
+while True:
+    # Input the name of the room to be searched 
+    roomNameToSearch = "IP_Adress"
 
     # Defines a variable that will hold the roomId 
     roomIdToGetMessages = None
@@ -34,28 +32,28 @@ for room in rooms:
         if(room["title"].find(roomNameToSearch) != -1):
 
             # Displays the rooms found using the variable roomNameToSearch (additional options included)
-            print ("Found room " + roomNameToSearch)
+            print ("Found rooms with the word " + roomNameToSearch)
             print(room["title"])
 
             # Stores room id and room title into variables
             roomIdToGetMessages = room["id"]
             roomTitleToGetMessages = room["title"]
-            print("Room: " + roomTitleToGetMessages + " available")
+            print("Found room : " + roomTitleToGetMessages)
             break
 
     if(roomIdToGetMessages == None):
-        print("Room " + roomNameToSearch + " not found")
-       
+        print("Sorry, I didn't find any room with " + roomNameToSearch + " in it.")
+        print("Please try again...")
     else:
         break
-    
 
-    # add  delay 
+
+# run the "bot" loop until manually stopped or an exception occurred
+while True:
+    # always add 1 second of delay to the loop to not go over a rate limit of API calls
     time.sleep(1)
 
-    
    
-    #  "max": 1  limits to get only the very last message in the room
     GetParameters = {
                             "roomId": roomIdToGetMessages,
                             "max": 1
@@ -78,48 +76,46 @@ for room in rooms:
     # store the array of messages
     messages = json_data["items"]
     # store the text of the first message in the array
-    message = messages[1]["text"]
-    print(message)
+    message = messages[0]["text"]
+    print("Received message: " + message)
+
     
+
    
-    
-#IP = "message"
-    
-       
- 
-Description = "message"
+            #set API URL
+    api_url = "https://10.10.20.48/restconf/data/ietf-interfaces:interfaces/interface=Loopback01"
 
-#set API URL
-api_url = "https://10.10.20.48/restconf/data/ietf-interfaces:interfaces/interface=Loopback99"
+    #dictionary variable
+    headers = { "Accept": "application/yang-data+json",
+                "Content-type":"application/yang-data+json"
+              }
+                            
+    #define authentication
+    basicauth = ("developer", "C1sco12345")
 
-#dictionary variable
-headers = { "Accept": "application/yang-data+json",
-            "Content-type":"application/yang-data+json"
-          }
-			
-#define authentication
-basicauth = ("developer", "C1sco12345")
+    #config for interface
+    yangConfig = { 
+            "ietf-interfaces:interface": { 
+                    "name": "Loopback01",
+                    "description": "First Loopback interface", 
+                    "type": "iana-if-type:softwareLoopback",
+                    "enabled": True,
+                    "ietf-ip:ipv4": { 
+                            "address": [
+                                    { "ip": message,
+                                    "netmask": "255.255.255.0" }
+                                    ] }, 
+                    "ietf-ip:ipv6": {}
+                            } 
+                    }
+                    
+    #send 'put' request
+    resp = requests.put(api_url, data=json.dumps(yangConfig), auth=basicauth, 
+    headers=headers, verify=False) 
+    if(resp.status_code >= 200 and resp.status_code <= 299):
+            print("STATUS OK: {}".format(resp.status_code)) 
+    else: 
+            print("Error code {}, reply: {}".format(resp.status_code, resp.json()))
 
-#config for interface
-yangConfig = { 
-	"ietf-interfaces:interface": { 
-		"name": "Description",
-		"description": "WHATEVER99", 
-		"type": "iana-if-type:softwareLoopback",
-		"enabled": True,
-		"ietf-ip:ipv4": { 
-			"address": [
-				{ "ip": "192.168.1.10",
-				"netmask": "255.255.255.0" }
-				] }, 
-		"ietf-ip:ipv6": {}
-			} 
-		}
-		
-#send 'put' request
-resp = requests.put(api_url, data=json.dumps(yangConfig), auth=basicauth, 
-headers=headers, verify=False) 
-if(resp.status_code >= 200 and resp.status_code <= 299):
-	print("STATUS OK: {}".format(resp.status_code)) 
-else: 
-	print("Error code {}, reply: {}".format(resp.status_code, resp.json()))
+                            
+                           
